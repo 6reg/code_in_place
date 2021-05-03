2e5422ff6ef8c9f56f61d6dee2753f5d58493838
greg mathias <mathiasgreg@gmail.com>
2021-05-03 09:58:16 -0400

    Average 2 Numbers

---
+++ average2numbers.py
@@ -0,0 +1,8 @@
+def main():
+    print("This program averages two numbers.")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    average = (num1 + num2) / 2
+    print("The average is", average)
+
+main()
